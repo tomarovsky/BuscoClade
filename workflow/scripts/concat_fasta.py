48d5e42c47903f908ca0de0cf5f8588d68335fa7
Tomarovsky <andrey.tomarovsky@gmail.com>
2026-03-28 00:33:50 +0700

    add log info

--- workflow/scripts/concat_fasta.py
+++ workflow/scripts/concat_fasta.py
@@ -2,21 +2,35 @@
 __author__ = "tomarovsky"
 import argparse
 from collections import defaultdict
-
 from Bio import SeqIO
-
 
 def main():
     sequence_map = defaultdict(str)
+    empty_files = 0
+    non_empty_files = 0
     for i in args.input:
-        for sequence in SeqIO.parse(i, "fasta"):
-            sequence_map[sequence.name] += str(sequence.seq)
+        records = list(SeqIO.parse(i, "fasta"))
+        if records:
+            non_empty_files += 1
+            for sequence in records:
+                sequence_map[sequence.name] += str(sequence.seq)
+        else:
+            empty_files += 1
+
+    print(f"Total input files: {len(args.input)}")
+    print(f"Non-empty files: {non_empty_files}")
+    print(f"Empty files: {empty_files}")
 
     outfile = open(args.output, "w")
     for key, value in sequence_map.items():
         outfile.write(f">{key}\n{value}\n")
     outfile.close()
 
+    lengths = set(len(v) for v in sequence_map.values())
+    if len(lengths) == 1:
+        print(f"Sequence length in output FASTA: {lengths.pop()}")
+    else:
+        print(f"WARNING: sequences have different lengths: {sorted(lengths)}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
